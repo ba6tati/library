e1d1b2b2d6432356ea1496d9b0822c6599826f4a
Ba6taTi <krasizh07@gmail.com>
2025-03-17 23:23:27 +0200

    fix: added author link to book entity

--- src/main/java/com/ba6tati/library/book/Book.java
+++ src/main/java/com/ba6tati/library/book/Book.java
@@ -3,10 +3,14 @@ package com.ba6tati.library.book;
 import java.sql.Date;
 import java.util.UUID;
 
+import com.ba6tati.library.author.Author;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,4 +24,8 @@ public class Book {
     private String title;
     private String description;
     private Date releaseDate;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
 }
