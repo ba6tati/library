8f7a9a4bb148e6b690c4f1c052990f2b2ad5838b
Ba6taTi <krasizh07@gmail.com>
2025-03-17 22:59:04 +0200

    initial commit

---
+++ src/test/java/com/ba6tati/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba6tati.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
