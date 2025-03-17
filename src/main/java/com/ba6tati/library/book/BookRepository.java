cadf4b87f24910bb2c3a2f5d991121aa1c059bde
Ba6taTi <krasizh07@gmail.com>
2025-03-17 23:31:28 +0200

    fix: name conventions for book repository

--- src/main/java/com/ba6tati/library/book/BookRepository.java
+++ src/main/java/com/ba6tati/library/book/BookRepository.java
@@ -7,9 +7,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
-    Optional<Book> findBookById(UUID id);
+    Optional<Book> findById(UUID id);
     
     // List<Book> findBookByAuthor(String author)
-    List<Book> findBookByTitleContaining(String title);
-    
+    List<Book> findByTitleContaining(String title);
+
 }
